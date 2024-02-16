bb2550cda568f0375d803dc58ec30a04fb9bb8e6
Rogstrug <karlomilos7@gmail.com>
2024-02-16 16:08:15 +0100

    Second

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    const socket = new WebSocket('ws://localhost:3000');
+    const socket = new WebSocket("ws://localhost:3000");
     var button = document.getElementById("send-button");
     var messagesDiv = document.getElementById("message-container");
-    button.addEventListener('click', (event)=>{
-        event.preventDefault();
-        let text = document.getElementById("message-input").value;
-        if (text != ""){
-            console.log(text);
-            socket.send(text);
-        }
+
+    var userName = prompt("Unesite tekst:");
+    if (userName == "" || userName.includes(":")) {
+      window.location.reload();
+    }
+
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      if (document.getElementById("message-input").value == "") return;
+      let text = userName + ": " + document.getElementById("message-input").value;
+      if (text != "") {
+        socket.send(text);
+      }
+      document.getElementById("message-input").value = "";
     });
 
-    socket.addEventListener('open', (event)=>{
-        console.log("Connected");
-    });
+    socket.addEventListener("message", (event) => {
+      const message = event.data;
+      const newMessageDiv = document.createElement("div");
 
-    socket.addEventListener('message', (event)=>{
-        messagesDiv.innerHTML += event.data + "<br>";
-        console.log("event.data: " + event.data);
-    })
-});
+      if (message.startsWith("You:")) {
+        newMessageDiv.classList.add("own-message");
+      }
+      else {
+        newMessageDiv.classList.add("other-message");
+      }
+      newMessageDiv.textContent = message;
+      messagesDiv.appendChild(newMessageDiv);
+    });
+  });
--- server.js
+++ server.js
@@ -1,38 +1,62 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 const server = require("http").createServer(app);
 const webSocket = require("ws");
 const path = require("path");
-const wss = new webSocket.Server({server: server});
 
-app.use("/", express.static(__dirname)
-);
+const wss = new webSocket.Server({ server: server });
 
+function writeStringToCSV(filename, data) {
+  fs.appendFile(__dirname + "/" + filename, data + "\n", 'utf8', (err) => {
+      if (err) {
+          console.error('Error writing to CSV file:', err);
+      } else {
+          console.log('String has been written to CSV file successfully.');
+      }
+  });
+}
 
-wss.on('connection', function connection(ws){
-    ws.send("Novi korisnik se uključio u čat");
-    ws.on('message', function incoming(message){
-        console.log(message.toString);
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === webSocket.OPEN) {
-              client.send(message.toString());
-            }
-        });
-    })
+app.use("/", express.static(__dirname));
+
+wss.on("connection", function connection(ws) {
+  let message = "Someone joined the chat!";
+  writeStringToCSV('messages.csv', message);
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === webSocket.OPEN) {
+      client.send(message);
+    }
+  });
+  ws.on("message", function incoming(message) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === webSocket.OPEN) {
+        writeStringToCSV('messages.csv', message.toString());
+        client.send(message.toString());
+      }
+      else {
+        var fullMessage = message.toString();
+        var parts = fullMessage.split(":", 2);
+        var newMessage = "You:" + parts[1];
+        writeStringToCSV('messages.csv', newMessage);
+        client.send(newMessage);
+      }
+    });
+  });
+
+  ws.on("close", function close() {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === webSocket.OPEN) {
+        let message = "Someone left the chat!";
+        client.send(message);
+      }
+    });
+  });
 });
 
-wss.on('close',function close(ws){
-    wss.clients.forEach(function each(client) {
-        if (client.readyState === webSocket.OPEN) {
-          client.send("Neki se korisnik skinuo s čata");
-        }
-    });
-})
-
-app.get('/',(req,res)=>{
-    res.sendFile(path + "index.html");
-})
+app.get("/", (req, res) => {
+  res.sendFile(path + "index.html");
+});
 
 server.listen(3000, () => {
-    console.log("Sever na portu 3000");
-})
+  console.log("Sever na portu 3000");
+});
